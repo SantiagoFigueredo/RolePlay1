22aba6bee2c91122f18d43f1acb16f78a805008c
Santiago Figueredo <66336675+SantiagoFigueredo@users.noreply.github.com>
2023-04-26 20:10:51 -0300

    Se crea personaje.cs
    Se agrega los atributos nombre, raza, nivel, salud, ataque, item y los metodos atacar y defender

---
+++ src/Library/Personaje.cs
@@ -0,0 +1,23 @@
+public class Personaje
+{
+    public string Nombre { get; set; }
+    public string Raza { get; set; }
+    public int Nivel { get; set; }
+    public int Salud { get; set; }
+    public int Ataque { get; set; }
+    public string Item { get; set; }
+
+    public void Atacar(Personaje otroPersonaje)
+    {
+        int dano = this.Ataque - otroPersonaje.Item.Defensa;
+        if (dano > 0)
+        {
+            otroPersonaje.Salud -= dano;
+        }
+    }
+
+    public void Defender(int dano)
+    {
+        this.Salud -= dano;
+    }
+}
