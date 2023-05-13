5dc7d93952ea9a6c0c49607fe34b65bb49868f82
Santiago Figueredo <66336675+SantiagoFigueredo@users.noreply.github.com>
2023-05-13 18:59:07 -0300

    Create Librodehechizos
    Cree una clase de libro de hechizos el cual contiene hechizos, se le pueden agregar y sacar y ademas tiene un metodo para obtener la lista de hechizos que tiene actualmente el libro

---
+++ src/Library/LibrodeHechizos.cs
@@ -0,0 +1,29 @@
+public class LibroDeHechizos
+{
+    // Atributos
+    public List<Hechizo> Hechizos { get; set; }
+
+    // Constructor
+    public LibroDeHechizos()
+    {
+        Hechizos = new List<Hechizo>();
+    }
+
+    // Método para agregar un hechizo al libro
+    public void AgregarHechizo(Hechizo hechizo)
+    {
+        Hechizos.Add(hechizo);
+    }
+
+    // Método para eliminar un hechizo del libro
+    public void EliminarHechizo(Hechizo hechizo)
+    {
+        Hechizos.Remove(hechizo);
+    }
+
+    // Método para obtener la lista de hechizos del libro
+    public List<Hechizo> ObtenerHechizos()
+    {
+        return Hechizos;
+    }
+}
