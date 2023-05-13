0c4663d940f666c066e238ef78c065732b70e842
Santiago Figueredo <66336675+SantiagoFigueredo@users.noreply.github.com>
2023-05-13 18:28:16 -0300

    Update enano
    Cree la clase en base a los cambios de la modificacion anterior, ademas agregue un atributo de resistencia, el cual consiste en agregarle un 50% extra de vida al personaje enano

--- src/Library/Enano.cs
+++ src/Library/Enano.cs
@@ -1,86 +1,15 @@
-public class Enano
+public class Enano : Personaje
 {
-    public string Nombre { get; set; }
-    public int Vida { get; set; }
-    public int Ataque { get; set; }
-    public int Defensa { get; set; }
-    public List<Elemento> Items { get; set; }
+    // Atributos adicionales de Enano
+    public int Resistencia { get; set; }
 
-    public Enano(string nombre, int vida, int ataque, int defensa)
+    // Constructor de Enano
+    public Enano(string nombre, int vida, int ataquePorDefecto, int resistencia) : base(nombre, vida, ataquePorDefecto)
     {
-        Nombre = nombre;
-        Vida = vida;
-        Ataque = ataque;
-        Defensa = defensa;
-        Items = new List<Elemento>();
+        Resistencia = resistencia;
+        Vida += Resistencia / 2; // Agregar la mitad de la resistencia a la vida del enano
     }
 
-    public void Equipar(Elemento item)
-    {
-        if (item != null)
-        {
-            if (item.Tipo == TipoElemento.Armadura)
-            {
-                // Si el enano ya tiene una armadura equipada, la desequipa.
-                Elemento armaduraEquipada = Items.FirstOrDefault(i => i.Tipo == TipoElemento.Armadura);
-                if (armaduraEquipada != null)
-                {
-                    Items.Remove(armaduraEquipada);
-                }
-            }
-
-            Items.Add(item);
-            Ataque += item.Ataque;
-            Defensa += item.Defensa;
-        }
-    }
-
-    public void Desequipar(Elemento item)
-    {
-        if (item != null && Items.Contains(item))
-        {
-            Items.Remove(item);
-            Ataque -= item.Ataque;
-            Defensa -= item.Defensa;
-        }
-    }
-
-    public int GetValorTotalAtaque()
-    {
-        int valorAtaque = Ataque;
-        foreach (Elemento item in Items)
-        {
-            valorAtaque += item.Ataque;
-        }
-        return valorAtaque;
-    }
-
-    public int GetValorTotalDefensa()
-    {
-        int valorDefensa = Defensa;
-        foreach (Elemento item in Items)
-        {
-            valorDefensa += item.Defensa;
-        }
-        return valorDefensa;
-    }
-
-    public void Atacar(Personaje personaje)
-    {
-        if (personaje != null)
-        {
-            int valorAtaqueEnano = GetValorTotalAtaque();
-            int valorDefensaPersonaje = personaje.GetValorTotalDefensa();
-            int danio = valorAtaqueEnano - valorDefensaPersonaje;
-            if (danio > 0)
-            {
-                personaje.Vida -= danio;
-            }
-        }
-    }
-
-    public void Curar()
-    {
-        Vida = VidaInicial;
-    }
+    // Métodos adicionales de Enano (si es necesario)
+    // ...
 }
